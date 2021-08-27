e41602d0e8b75ad282d1c20942996ce8b3b19387
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-08-26 21:44:40 -0400

    Update DEBUG.lua

--- Contents/mods/Super Weird Helicopters/media/lua/client/DEBUG.lua
+++ Contents/mods/Super Weird Helicopters/media/lua/client/DEBUG.lua
@@ -2,34 +2,14 @@ Events.OnCustomUIKey.Add(function(key)
 	if getDebug() and (eHelicopterSandbox.config.debugTests==true) then
 		if key == Keyboard.KEY_1 then DEBUG_TESTS.eHeliEventsOnSchedule()--DEBUG_TESTS.testAllLines()
 		elseif key == Keyboard.KEY_2 then DEBUG_TESTS.raiseTheDead()
-		elseif key == Keyboard.KEY_3 then
-			local heli1 = getFreeHelicopter("Spiffocopter")
-			heli1:launch()
-			DEBUG_TESTS.moveHeliCloser(heli1)
-
-		elseif key == Keyboard.KEY_4 then --DEBUG_TESTS.launch_jet()
-			local heli2 = getFreeHelicopter("TISCreamery")
-			heli2:launch()
-			DEBUG_TESTS.moveHeliCloser(heli2)
-
-		elseif key == Keyboard.KEY_5 then --DEBUG_TESTS.launch_aid_chopper()
-			local heli3 = getFreeHelicopter("IRS")
-			heli3:launch()
-			DEBUG_TESTS.moveHeliCloser(heli3)
-
-		elseif key == Keyboard.KEY_6 then --DEBUG_TESTS.launch_news_chopper()
-			local heli3 = getFreeHelicopter("UFOTourists")
-			heli3:launch()
-			DEBUG_TESTS.moveHeliCloser(heli3)
-
-		elseif key == Keyboard.KEY_7 then --DEBUG_TESTS.launch_news_chopper()
-			local heli3 = getFreeHelicopter("UFORednecks")
-			heli3:launch()
-			DEBUG_TESTS.moveHeliCloser(heli3)
-
-		elseif key == Keyboard.KEY_8 then DEBUG_TESTS.launch_increasingly_helpful()
-		elseif key == Keyboard.KEY_9 then DEBUG_TESTS.launch_police_heli()
-		elseif key == Keyboard.KEY_0 then DEBUG_TESTS.launchBaseHeli()
+		elseif key == Keyboard.KEY_3 then DEBUG_TESTS.ToggleAllCrash()
+		elseif key == Keyboard.KEY_4 then DEBUG_TESTS.ToggleMoveHeliCloser()
+		elseif key == Keyboard.KEY_5 then DEBUG_TESTS.launchHeliTest("Spiffocopter")
+		elseif key == Keyboard.KEY_6 then DEBUG_TESTS.launchHeliTest("TISCreamery")
+		elseif key == Keyboard.KEY_7 then DEBUG_TESTS.launchHeliTest("IRS")
+		elseif key == Keyboard.KEY_8 then DEBUG_TESTS.launchHeliTest("UFOTourists")
+		elseif key == Keyboard.KEY_9 then DEBUG_TESTS.launchHeliTest("UFORednecks")
+		elseif key == Keyboard.KEY_0 then DEBUG_TESTS.launchHeliTest()
 		end
 	end
 end)
