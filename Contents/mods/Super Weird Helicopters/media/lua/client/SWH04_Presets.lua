a90a0e31384b530a52336f2f5cf48876bf9e9781
ChuckTheJittai <6071161+ChuckTheSheep@users.noreply.github.com>
2021-11-04 21:04:50 -0400

    Refactor preset loading to rollover variables

--- Contents/mods/Super Weird Helicopters/media/lua/client/SWH04_Presets.lua
+++ Contents/mods/Super Weird Helicopters/media/lua/client/SWH04_Presets.lua
@@ -34,103 +34,77 @@ eHelicopter_PRESETS["id_name"] = {
 ]]
 
 
+
 eHelicopter_PRESETS["Spiffocopter"] = {
 	presetProgression = {
 		["Spiffocopter_inviteOnly"] = 0,
 		["Spiffocopter_partyTime"] = 0.05,
-	}
-}
-
-eHelicopter_PRESETS["Spiffocopter_inviteOnly"] = {
+	},
 	crashType = {"Bell206SpiffoFuselage"},
 	hoverOnTargetDuration = {2000,2250},
 	scrapAndParts = {"Bell206SpiffoTail", "EHE.Bell206HalfSkirt", "EHE.Bell206RotorBlade", 2, "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10},
 	eventSoundEffects = {["flightSound"] = "eHelicopter"},
-	crew = {"SpiffoBoss",100,0,
-			"SpiffoBoss",10,0,
-			"SpiffoBoss",1,0,},
 	dropPackages = {"SpiffoBurger"},
 	announcerVoice = "Spiffo",
 	dropItems = {["SWH.SpiffoInvite"]=250},
+	crew = {"SpiffoBoss",100,0,
+			"SpiffoBoss",10,0,
+			"SpiffoBoss",1,0,},
 }
+
+eHelicopter_PRESETS["Spiffocopter_inviteOnly"] = {}
 
 eHelicopter_PRESETS["Spiffocopter_partyTime"] = {
-	crashType = {"Bell206SpiffoFuselage"},
-	hoverOnTargetDuration = {2000,2250},
-	scrapAndParts = {"Bell206SpiffoTail", "EHE.Bell206HalfSkirt", "EHE.Bell206RotorBlade", 2, "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10},
-	crew = {"SpiffoBoss",100,0,
-			"SpiffoBoss",10,0,
-			"SpiffoBoss",1,0,},
 	addedFunctionsToEvents = {["OnHover"] = eHelicopter_dropCrewOff,},
-	eventSoundEffects = {["flightSound"] = "eHelicopter"},
-	dropPackages = {"SpiffoBurger"},
-	announcerVoice = "Spiffo",
-	dropItems = {["SWH.SpiffoInvite"]=250},
 }
 
+
+
 eHelicopter_PRESETS["UFO"] = {
-	presetRandomSelection = {"UFOTourists",4,"UFORednecks",1,"UFOFratBoys",1,}
+	presetRandomSelection = {"UFOTourists",4,"UFORednecks",1,"UFOFratBoys",1,},
+	crashType = {"UFO"},
+	hoverOnTargetDuration = {2000,2250},
+	speed = 1.3,
+	topSpeedFactor = 2,
+	addedFunctionsToEvents = {["OnHover"] = eHelicopter_dropCrewOff,},
+	scrapAndParts = false,
+	addedCrashChance = 33,
+	flightHours = {19,24},
+	announcerVoice = "Aliens",
+	eventMarkerIcon = "media/ui/markerUFO.png",
+	eventSoundEffects = {["flightSound"] = "AlienUFOFlight"},
 }
 
 eHelicopter_PRESETS["UFOTourists"] = {
-	crashType = {"UFO"},
-	hoverOnTargetDuration = {2000,2250},
 	crew = {"AlienTourist",100,0,
 			"AlienTourist",75,0,
 			"AlienTourist",30,0,
 			"AlienTourist",15,0,
 			"AlienTourist",5,0, },
-	speed = 1.3,
-	topSpeedFactor = 2,
-	addedFunctionsToEvents = {["OnHover"] = eHelicopter_dropCrewOff,},
-	scrapAndParts = false,
-	eventSoundEffects = {["flightSound"] = "AlienUFOFlight"},
-	addedCrashChance = 33,
-	flightHours = {19,24},
-	announcerVoice = "Aliens",
-	eventMarkerIcon = "media/ui/markerUFO.png",
 }
 
 eHelicopter_PRESETS["UFORednecks"] = {
 	crashType = {"UFORedneck"},
-	hoverOnTargetDuration = {2000,2250},
 	crew = {"AlienRedneck",100,0,
 			"AlienRedneck",75,0,
 			"AlienRedneck",30,0,
 			"AlienRedneck",15,0,
 			"AlienRedneck",5,0, },
-	speed = 1.3,
-	topSpeedFactor = 2,
-	addedFunctionsToEvents = {["OnHover"] = eHelicopter_dropCrewOff,},
-	scrapAndParts = false,
-	eventSoundEffects = {["flightSound"] = "AlienUFOFlight"},
-	addedCrashChance = 33,
-	flightHours = {19,24},
-	announcerVoice = "Aliens",
-	eventMarkerIcon = "media/ui/markerUFO.png",
 }
 
 eHelicopter_PRESETS["UFOFratBoys"] = {
-	crashType = {"UFO"},
-	hoverOnTargetDuration = {2000,2250},
 	crew = {"AlienBeefo",100,0,
 			"AlienBeefo",75,0,
 			"AlienBeefo",30,0,
 			"AlienBeefo",15,0,
 			"AlienBeefo",5,0, },
-	speed = 1.3,
-	topSpeedFactor = 2,
-	addedFunctionsToEvents = {["OnHover"] = eHelicopter_dropCrewOff,},
-	scrapAndParts = false,
 	eventSoundEffects = {
 		["flightSound"] = "AlienUFOFlight",
 		["additionalFlightSound"] = "assBlastUSA"
 		},
-	addedCrashChance = 33,
-	flightHours = {19,24},
 	announcerVoice = "FratAliens",
-	eventMarkerIcon = "media/ui/markerUFO.png",
 }
+
 
 
 eHelicopter_PRESETS["IRS"] = {
@@ -138,80 +112,50 @@ eHelicopter_PRESETS["IRS"] = {
 		["IRS_Wave1"] = 0,
 		["IRS_Wave2"] = 0.2,
 		["IRS_Wave3"] = 0.5,
-	}
+	},
+	crashType = {"Bell206IRSFuselage"},
+	hoverOnTargetDuration = {2000,2250},
+	crashType = {"Bell206IRSFuselage"},
+	hoverOnTargetDuration = {2000,2250},
+	scrapAndParts = {"Bell206IRSTail", "EHE.Bell206HalfSkirt", "EHE.Bell206RotorBlade", 2, "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10},
+	attackDelay = 1000,
+	attackSpread = 4,
+	speed = 0.09,
+	attackHitChance = 65,
+	attackDamage = 35,
+	hostilePreference = "IsoPlayer",
+	dropItems = {["SWH.TenFortyForm"]=400},
+	eventSoundEffects = {
+		["attackSingle"] = "eHeli_bolt_action_fire_single",
+		["attackLooped"] = "eHeli_bolt_action_fire_single",
+		["flightSound"] = "eHelicopter",
+	},
+	addedFunctionsToEvents = {["OnHover"] = eHelicopter_dropCrewOff,},
+	announcerVoice = "IRS",
 }
 
 eHelicopter_PRESETS["IRS_Wave1"] = {
-	crashType = {"Bell206IRSFuselage"},
-	hoverOnTargetDuration = {2000,2250},
-	scrapAndParts = {"Bell206IRSTail", "EHE.Bell206HalfSkirt", "EHE.Bell206RotorBlade", 2, "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10},
 	crew = {"TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0,
 			"TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0,},
-	attackDelay = 1000,
-	attackSpread = 4,
-	speed = 0.09,
-	attackHitChance = 65,
-	attackDamage = 35,
-	hostilePreference = "IsoPlayer",
-	dropItems = {["SWH.TenFortyForm"]=400},
-	eventSoundEffects = {
-		["attackSingle"] = "eHeli_bolt_action_fire_single",
-		["attackLooped"] = "eHeli_bolt_action_fire_single",
-		["flightSound"] = "eHelicopter",
-		},
-	addedFunctionsToEvents = {["OnHover"] = eHelicopter_dropCrewOff,},
-	announcerVoice = "IRS",
 }
 
 eHelicopter_PRESETS["IRS_Wave2"] = {
-	crashType = {"Bell206IRSFuselage"},
-	hoverOnTargetDuration = {2000,2250},
-	scrapAndParts = {"Bell206IRSTail", "EHE.Bell206HalfSkirt", "EHE.Bell206RotorBlade", 2, "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10},
 	crew = {"TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0,
 			"TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0,
 			"TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0,
 			"TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0,},
-	attackDelay = 1000,
-	attackSpread = 4,
-	speed = 0.09,
-	attackHitChance = 65,
-	attackDamage = 35,
-	hostilePreference = "IsoPlayer",
-	dropItems = {["SWH.TenFortyForm"]=400},
-	eventSoundEffects = {
-		["attackSingle"] = "eHeli_bolt_action_fire_single",
-		["attackLooped"] = "eHeli_bolt_action_fire_single",
-		["flightSound"] = "eHelicopter",
-	},
-	addedFunctionsToEvents = {["OnHover"] = eHelicopter_dropCrewOff,},
-	announcerVoice = "IRS",
 }
 
 eHelicopter_PRESETS["IRS_Wave3"] = {
-	crashType = {"Bell206IRSFuselage"},
-	hoverOnTargetDuration = {2000,2250},
-	scrapAndParts = {"Bell206IRSTail", "EHE.Bell206HalfSkirt", "EHE.Bell206RotorBlade", 2, "EHE.Bell206TailBlade", 2, "Base.ScrapMetal", 10},
 	crew = {"TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0,
 			"TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0,
 			"TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0, "TaxMan",100,0,
 			"TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0,
 			"TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0,
 			"TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0, "TaxMan",50,0,},
-	attackDelay = 1000,
-	attackSpread = 4,
-	speed = 0.09,
-	attackHitChance = 65,
-	attackDamage = 35,
-	hostilePreference = "IsoPlayer",
-	dropItems = {["SWH.TenFortyForm"]=400},
-	eventSoundEffects = {
-		["attackSingle"] = "eHeli_bolt_action_fire_single",
-		["attackLooped"] = "eHeli_bolt_action_fire_single",
-		["flightSound"] = "eHelicopter",
-	},
-	addedFunctionsToEvents = {["OnHover"] = eHelicopter_dropCrewOff,},
-	announcerVoice = "IRS",
 }
+
+
 
 eHelicopter_PRESETS["TISCreamery"] = {
 	crashType = {"TISIceCreamTruck"},
