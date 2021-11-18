116d3a17f65da786956b174c075230d3ea5810dd
ChuckTheJittai <6071161+ChuckTheSheep@users.noreply.github.com>
2021-11-17 22:34:09 -0500

    Update ExpandedHelicopter_Camping.lua

--- Contents/mods/Super Weird Helicopters/media/lua/server/Camping/ExpandedHelicopter_Camping.lua
+++ Contents/mods/Super Weird Helicopters/media/lua/server/Camping/ExpandedHelicopter_Camping.lua
@@ -7,4 +7,17 @@ campingLightFireType.TenFortyForm = 5/60.0
 campingLightFireType.SpiffoInvite = 5/60.0
 campingLightFireType.StrangeNewspaper = 5/60.0
 
+---This has to be here for both mods to work for some reason
+-- The value is the number of hours of fuel added to a fire.
+campingFuelType.NoticeFlyer = 5/60.0
+campingFuelType.PreventionFlyer = 5/60.0
+campingFuelType.QuarantineFlyer = 5/60.0
+campingFuelType.EmergencyFlyer = 5/60.0
+campingFuelType.EvacuationFlyer = 5/60.0
+-- Types of items that can be used with a lighter/matches to start a fire.
+campingLightFireType.NoticeFlyer = 5/60.0
+campingLightFireType.PreventionFlyer = 5/60.0
+campingLightFireType.QuarantineFlyer = 5/60.0
+campingLightFireType.EmergencyFlyer = 5/60.0
+campingLightFireType.EvacuationFlyer = 5/60.0
 
