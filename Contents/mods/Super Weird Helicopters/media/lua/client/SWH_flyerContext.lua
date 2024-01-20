f546f66ba7e7ce0e5ec6ae24d17477b2bc15d6ba
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-20 00:06:20 -0500

    Create SWH_flyerContext.lua

---
+++ Contents/mods/Super Weird Helicopters/media/lua/client/SWH_flyerContext.lua
@@ -0,0 +1,5 @@
+local paperContext = require "expandedHeliFlyersContextMenu"
+
+paperContext.registerType("SWH.TenFortyForm")
+paperContext.registerType("SWH.SpiffoInvite")
+paperContext.registerType("SWH.StrangeNewspaper")
